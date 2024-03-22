9633425a59bf76269ab5646940ace1542bde2254
Lucas Amaral <lopesdoamaral@gmail.com>
2024-03-22 09:20:10 -0300

    Cucumber preprocessor configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
+const cucumber = require('cypress-cucumber-preprocessor').default;
 const {defineConfig} = require('cypress');
 require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber());
+    },
+    specPattern: 'cypress/e2e/features/*.feature',
     baseUrl: process.env.BASE_URL || 'https://magento.softwaretestingboard.com',
   },
 });
